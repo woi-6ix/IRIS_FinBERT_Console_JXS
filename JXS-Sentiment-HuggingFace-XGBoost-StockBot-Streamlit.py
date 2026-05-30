752720fe4f7e8610487d958b4a41f1f64b570b1b
Sathwik <109254825+woi-6ix@users.noreply.github.com>
2026-05-30 01:05:06 -0400

    Update description formatting in info message

--- JXS-Sentiment-HuggingFace-XGBoost-StockBot-Streamlit.py
+++ JXS-Sentiment-HuggingFace-XGBoost-StockBot-Streamlit.py
@@ -363,7 +363,7 @@ with st.sidebar.form("analysis_form"):
     run_button = st.form_submit_button("Run IRIS Scan")
 
 if not run_button:
-    st.info("IRIS, named after the Greek messenger goddess and the colors of the rainbow, is an stock analysis dashboard that delivers real-time news sentiment insights through Hugging Face's FinBERT NLP, combining market data, technical indicators, and XGBoost forecasting to help users interpret potential stock trends.
+    st.info("**IRIS**, named after the Greek messenger goddess and the colors of the rainbow, is an stock analysis dashboard that delivers real-time news sentiment insights through Hugging Face's FinBERT NLP, combining market data, technical indicators, and XGBoost forecasting to help users interpret potential stock trends.
     
             Enter a stock ticker in sidebar at top left corner. Click **Run IRIS Scan** to start.")
     st.stop()
